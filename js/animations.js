2e48e81a2e41e43a3aa778801ec7f969eb9f92c0
Konstantin <talakvenolo@gmail.com>
2022-12-20 06:03:02 +0300

    Восьмая версия проекта

--- js/animations.js
+++ js/animations.js
@@ -22,7 +22,6 @@ function initIncreaseNumberAnimation() {
     increaseNumberAnimationStep(0, element, 5000);
 }
 
-initIncreaseNumberAnimation();
 
 document.querySelector('#budget').addEventListener('change', function handleSelectChange(event) {
     if (event.target.value === 'other') {
@@ -43,3 +42,47 @@ document.querySelector('#budget').addEventListener('change', function handleSele
     document.querySelector('#form form').removeChild(otherInput); // Задание 4
     }
 });
+
+function updateScroll() {
+    if (window.scrollY > 0) {
+    document.querySelector('header').classList.add('header__scrolled');
+    } else {
+    document.querySelector('header').classList.remove('header__scrolled');
+    }
+
+    // Запуск анимации увеличения числа
+    let windowBottomPosition = window.scrollY + window.innerHeight;
+    let countElementPosition = document.querySelector('.features__clients-count').offsetTop;
+    if (windowBottomPosition >= countElementPosition && !animationInited) {
+    animationInited = true;
+    initIncreaseNumberAnimation();
+    }
+}
+
+window.addEventListener('scroll', updateScroll);
+
+let animationInited = false;
+
+function addSmoothScroll(anchor) {
+    anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+    behavior: 'smooth'
+    });
+    });
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    addSmoothScroll(anchor);
+});
+
+function onLinkClick(event) {
+    event.preventDefault();
+    document.querySelector(event.target.getAttribute('href')).scrollIntoView({
+    behavior: 'smooth'
+    });
+}
+
+addSmoothScroll(document.querySelector('.more-button'));
+
+
